48eeba3e9ef9dc14c77ed8c7fbf152d55d45bfe0
lampslave <lampslave@gmail.com>
2015-02-17 15:51:41 +0300

    fix packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-js-urlresolver',
     version='0.1',
-    description='',
-    long_description='',
+    packages=['js_urlresolver'],
+    include_package_data=True,
+    license='',
+    description='Simple access to Django\'s resolve and reverse functions in JS',
+    long_description=README,
     url='https://github.com/hovel/django-js-urlresolver',
     author='',
     author_email='',
-    license='',
-    packages=['js_urlresolver'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        # 'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+
 )
