c10b1fd6c93b8e7217199ce49ef008d8f8bc7dd6
lampslave <lampslave@gmail.com>
2017-08-24 14:42:53 +0300

    update testproject for django 1.11

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,17 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'testproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$',
         TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^test/(?P<test_1>\w+)/(?P<test_2>\w+)/(?P<test_3>\w+)/$',
         TemplateView.as_view(template_name='base.html'), name='test'),
     url(r'^test/(?P<test_21>\w+)/(?P<test_22>\w+)/(?P<test_23>\w+)/$',
         TemplateView.as_view(template_name='base.html'), name='test2'),
-)
+]
